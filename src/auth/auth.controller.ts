9c14e6da075e2416201e01ea016468af5c81d8fd
Konrad Kolasa <kolasakonrad1@gmail.com>
2023-10-01 16:35:36 +0200

    feat(auth): create nest project and implement jwt auth flow

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+import { Tokens } from './types';
+import { RtGuard } from 'src/common/guards';
+import {
+  GetCurrentUser,
+  GetCurrentUserId,
+  PublicRoute,
+} from 'src/common/decorators';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @PublicRoute()
+  @Post('local/signup')
+  @HttpCode(HttpStatus.CREATED)
+  signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
+    return this.authService.signupLocal(dto);
+  }
+
+  @PublicRoute()
+  @Post('local/signin')
+  @HttpCode(HttpStatus.OK)
+  signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
+    return this.authService.signinLocal(dto);
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  logout(@GetCurrentUserId() userId: number) {
+    return this.authService.logout(userId);
+  }
+
+  @PublicRoute()
+  @UseGuards(RtGuard)
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  refreshToken(
+    @GetCurrentUser('refreshToken') rt: string,
+    @GetCurrentUserId() userId: number,
+  ) {
+    return this.authService.refreshToken(userId, rt);
+  }
+}
